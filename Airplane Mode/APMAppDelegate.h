6ec55fc78cdab022d7f4ef1a2438afd20444eb54
Matt Rubin <mattrubin@me.com>
2014-01-22 14:12:24 -0800

    New Xcode project

---
+++ Airplane Mode/APMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APMAppDelegate.h
+//  Airplane Mode
+//
+//  Created by Matt Rubin on 1/22/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Airplane Mode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Airplane Mode
+//
+//  Created by Matt Rubin on 1/22/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APMAppDelegate class]));
+    }
+}
